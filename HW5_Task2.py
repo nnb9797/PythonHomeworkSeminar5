f4730025ee3bc0e2fdee0e6392d95bc48222d286
nnb9797 <108178648+nnb9797@users.noreply.github.com>
2022-10-15 23:08:00 +0700

    Update HW5_Task2.py

--- HW5_Task2.py
+++ HW5_Task2.py
@@ -1,30 +1,49 @@
 # 2). Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
 
+def rle_encoding(text: str) -> str:
+    text_encoded = ''
+    count = 1
+    for i in range(len(text) - 1):
+        if text[i] == text[i + 1]:
+            count += 1
+        else:
+            if count > 0:
+                text_encoded = text_encoded + str(count) + text[i]
+            else:
+                text_encoded = text_encoded + text[i]
+            count = 1
+    if count > 1:
+        text_encoded = text_encoded + str(count) + text[i]
+    else:
+        text_encoded = text_encoded + text[i+1]
+    return text_encoded
+
+def rle_decoding(text: str) -> str:
+    text_decoded = ''
+    number = ''
+    for i in range(len(text)):
+        if text[i].isdigit():
+            number += text[i]
+        else:
+            if not number == '':
+                text_decoded = text_decoded + int(number) * text[i]
+            else:
+                text_decoded = text_decoded + text[i]
+            number = ''
+    return text_decoded
+
 
 with open("input.txt", "r") as data:
-    my_text = data.read()
+    text_str = data.read()
 
-print(f"входные данные: {my_text}")
+text_encoded = rle_encoding(text_str)
 
-def encode_rle(inp_symbols):
-    str_code = ""
-    prev_char = ""
-    count = 1
-    for char in inp_symbols:
-        if char != prev_char:
-            if prev_char:
-                str_code += str(count) + prev_char
-            count = 1
-            prev_char = char
-        else:
-            count += 1
-    return str_code
+print(f"Входные данные:{text_str}")
+print(f"Сжатые данные:{text_encoded}")
 
-
-str_code = encode_rle(my_text)
-print(f"выходные данные: {str_code}")
-new_data = str_code
+text_initial = rle_decoding(text_encoded)
+print(f"Восстановленные данные: {text_initial}")
 
 with open("output.txt", "w") as data:
-    data.write(new_data)
+     text_initial = data.write(text_encoded)
